30fdb91c1c8a2312486174417a0317f9f7f0cdc9
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-12 23:44:27 +0000

    Converts the function parameter to a strings and returns the string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A to_str function module.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string value of the input parameter
+    """
+    return f"{n}"
