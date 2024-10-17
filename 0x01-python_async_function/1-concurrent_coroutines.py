7cec5e16e10e2af1b36718707e5f634e68681d87
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-17 02:35:32 +0000

    Intergrated type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,11 @@ A wait_n function module.
 
 import asyncio
 import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List:
     """
     Returns the list of all the delays
     (float values) in ascending order.
