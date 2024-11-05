afd9831984bcbcb15aa44eb16cd769e74f7705fa
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-05 22:27:01 +0000

    Test that GithubOrgClient.org returns the correct value.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+A TestGithubOrgClient class module
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    A unittest subclass
+    """
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')  # Mock get_json in the client module
+    def test_org(self, org_name, mock_get_json):
+        """
+        Define the return value for the mock
+        """
+        mock_get_json.return_value = {"name": org_name}
+
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient(org_name)
+
+        # Call the org method
+        result = client.org
+
+        # Test that get_json was called
+        # once with the expected argument
+        mock_get_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+                )
+
+        # Test that the returned value is as expected
+        self.assertEqual(result, {"name": org_name})
+
+
+if __name__ == '__main__':
+    unittest.main()
