f43d05c81aa629096d353cc5a8770ca7646b9ee3
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-08 14:02:58 +0000

    Fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
 """
-A TestGithubOrgClient class module
+Unit tests for the client.GithubOrgClient class.
 """
 
-import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from typing import Dict
+import unittest
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """
-    A unittest subclass
+    Test cases for GithubOrgClient class methods.
     """
+
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
-    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    @patch('client.get_json', new_callable=PropertyMock)
     def test_org(self, org_name: str, mock_get_json: PropertyMock) -> None:
         """
-        Define the return value for the mock
+        Test that GithubOrgClient.org returns the
+        correct value and that get_json is called
+        once with the expected argument.
         """
         mock_get_json.return_value = {"name": org_name}
 
-        # Create an instance of GithubOrgClient
         client = GithubOrgClient(org_name)
-
-        # Call the org method
         result: Dict = client.org
 
-        # Test that get_json was called
-        # once with the expected argument
-        mock_get_json.assert_called_once_with()
-
-        # Test that the returned value is as expected
+        mock_get_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+                )
         self.assertEqual(result, {"name": org_name})
